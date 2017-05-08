8092e3cbaf3400362888fcabdd95422bd67a2ec0
mpfleger <michaela.pfleger@elements.at>
2017-05-08 16:47:01 +0200

    Added Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Drawer from 'material-ui/Drawer';
 import AppBar from 'material-ui/AppBar';
 import transitions from 'material-ui/styles/transitions';
 import MenuItem from 'material-ui/MenuItem';
+import PlaylistIcon from 'material-ui/svg-icons/av/playlist-play';
+import RadioIcon from 'material-ui/svg-icons/av/radio';
+import SettingsIcon from 'material-ui/svg-icons/action/settings';
+import InfoIcon from 'material-ui/svg-icons/action/info';
+import Settings from './routes/Settings.jsx';
+import Program from './routes/Program.jsx';
+import About from './routes/About.jsx';
+import Stations from './routes/Stations.jsx';
 
 const styles = {
   content: {
@@ -11,7 +20,38 @@ const styles = {
     maxWidth: 800,
     transition: transitions.easeOut(null, 'padding-left', null),
   },
+  menuLink: {
+    textDecoration: 'none',
+  },
 };
+
+const routes = [
+  {
+    link: '/',
+    exact: true,
+    component: Program,
+    title: 'Programm',
+    icon: <PlaylistIcon />,
+  },
+  {
+    link: '/stations',
+    component: Stations,
+    title: 'Sender',
+    icon: <RadioIcon />,
+  },
+  {
+    link: '/settings',
+    component: Settings,
+    title: 'Einstellungen',
+    icon: <SettingsIcon />,
+  },
+  {
+    link: '/about',
+    component: About,
+    title: 'Info',
+    icon: <InfoIcon />,
+  },
+];
 
 export default class App extends React.Component {
   constructor(props) {
@@ -53,22 +93,39 @@ export default class App extends React.Component {
     });
   }
 
+  closeDrawer() {
+    if (!this.state.drawer.docked) {
+      this.toggleDrawer();
+    }
+  }
+
   render() {
     const paddingLeft = (this.state.drawer.docked ? 256 : 0) + 16;
 
     return <MuiThemeProvider>
-            <div>
-                <AppBar title={this.state.station.name} style={{ paddingLeft }}
-                        iconStyleLeft={{ display: this.state.drawer.docked ? 'none' : 'block' }}
-                        onLeftIconButtonTouchTap={() => this.toggleDrawer()} />
-                <Drawer open={this.state.drawer.open} docked={this.state.drawer.docked}
-                onRequestChange={() => this.toggleDrawer()}>
-                    <MenuItem>Home</MenuItem>
-                </Drawer>
-               <div style={{ ...styles.content, paddingLeft }}>
-                   Content
-               </div>
-            </div>
+            <Router>
+              <div>
+                  <AppBar title={this.state.station.name} style={{ paddingLeft }}
+                          iconStyleLeft={{ display: this.state.drawer.docked ? 'none' : 'block' }}
+                          onLeftIconButtonTouchTap={() => this.toggleDrawer()} />
+                  <Drawer open={this.state.drawer.open} docked={this.state.drawer.docked}
+                  onRequestChange={() => this.toggleDrawer()}>
+
+                    {routes.map(route =>
+                      <Link key={route.link} to={ route.link } style={styles.menuLink}>
+                        <MenuItem primaryText={route.title} onTouchTap={() => this.closeDrawer()}
+                                  leftIcon={route.icon}></MenuItem>
+                      </Link>)}
+
+                  </Drawer>
+                 <div style={{ ...styles.content, paddingLeft }}>
+                   {routes.map(route => (
+                       <Route exact={route.exact} key={route.link}
+                              path={route.link} component={route.component} />
+                   ))}
+                 </div>
+              </div>
+            </Router>
         </MuiThemeProvider>;
   }
 }
